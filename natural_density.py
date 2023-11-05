e4d4b89b1fa354418b962d265de266c64a07c129
WuPhil <32814443+WuPhil@users.noreply.github.com>
2023-11-05 00:49:51 -0400

    Add files via upload

--- natural_density.py
+++ natural_density.py
@@ -14,52 +14,51 @@
 # okay lemme just do the problem and to the talking later
 
 # lets start with 1 k and go up:
-base = 1000
-
-onem = list(range(base))
-tenm = list(range(10*base))
-hunm = list(range(100*base))
+base = 100
 
 # add more fidelity around the millions
-base = base * 1000
-mills = [list(range(i*base)) for i in range(1,1000)]
+mills = [list(range((i-1)*base, i*base)) for i in range(1,1000)]
 
-base = base * 1000
-bills = [list(range(i * base)) for i in range(1,100)]
-
-dofs = list(range(1000))
+#billions will break ram and other specs for sure dont do it
+# unless you got superpc or smth
+#base = base * 1000
+# bills = [list(range(i * base)) for i in range(1,100)]
 
 print("starting with square sumsets: we know 2 should tend to 0, 3 uses legendre")
-print("for 1-(1/8 + 1/32 + ... + 1/8*4^n) = 1-1/4(\sum 1/2^k) = 3/4 supposedly")
+print("for 1-(1/8 + 1/32 + ... + 1/8*4^n) = 1-1/4(sum 1/2^k) = 3/4 supposedly")
 
 print("so lets run the tests:")
 p = 2
 dof = 2
 
-for b in mills:
-    sqs = [c**2 for c in b]
-    S = set()
-    for i in sqs:
-        for j in sqs:
-            set.add(i+j)
-    print("total found" , len(set), "for", b)
-    print("ratio:", len(set)/b)
+# Try 2: Dynamic Programming Approach:
+# don't want to waste compute resources.
+# idea: only add to the set, don't just redefine everything
+# makes life so much easier for the computer at least
 
-for count in bills:
-    sqs = [c**2 for c in count]
+# all the squares go in sqs
+sqs = []
+# sum of square set in S
+S = set()
+
+for b in range(len(mills)):
+    sqs.extend([c**2 for c in mills[b]])
+    for i in range(len(sqs)):
+        for j in range(b*base, (b+1)*base):
+            S.add(sqs[i]+sqs[j])
+    print("total found" , len(S), "for", b*base)
+    print("real ratio:", len(S)/ (((b+1)*base)**2) )
+
+# cubes, if i get there
+
+'''
+for count in mills:
+    sqs = [c**3 for c in count]
     S = set()
     for a in sqs:
         for b in sqs:
             for c in sqs:
-                set.add(i+j+c)
-    print("total found" , len(set), "for", count)
-    print("ratio:", len(set)/count)
-            
-    
-
-
-
-
-p = int(input("power?\n"))
-
-
+                S.add(i+j+c)
+    print("total found", len(S), "for", len(count))
+    print("ratio:", len(S)/len(count))
+'''
